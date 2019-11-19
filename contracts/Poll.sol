b2b0f6f49e5e8c65bc0ac9240a103a14964921b1
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-19 01:27:48 -0500

    figured out better solution for compiler version, extended contract while exploring solidity 0.5

--- contracts/Poll.sol
+++ contracts/Poll.sol
@@ -1,5 +1,24 @@
-pragma solidity ^0.5.12;
+pragma solidity >=0.4.0 <0.6.0;
 
 contract Poll{
-    string public contendor;
+    //model class for contestant//
+    struct Contestant {
+        uint id;
+        string name;
+        string genre;
+        uint votes;
+    }
+
+    //mapping to fetch contestant details//
+    mapping(uint => Contestant) public contestants;
+
+    //varialble to keep track of number of contestants - because of the way solidity work//
+    uint public contestantCount;
+
+    //function to add a contestant//
+    function addNewContestant (string memory _name, string memory _genre) private {
+        //increament the value of contestantCount//
+        contestantCount  ++;
+        contestants[contestantCount] = Contestant(contestantCount, _name, _genre, 0);
+    }
 }
--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   // See <http://truffleframework.com/docs/advanced/configuration>
-//   // for more about customizing your Truffle configuration!
-//   networks: {
-//     development: {
-//       host: "127.0.0.1",
-//       port: 7545,
-//       network_id: "*" // Match any network id
-//     },
-    
-//   }
-// };
-
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -20,13 +7,8 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
-    },
-    compilers: {
-      solc: {
-        version: "0.5.12" // ex:  "0.4.20". (Default: Truffle's installed solc)
-      }
-    }
+    
   }
 };
+
+
