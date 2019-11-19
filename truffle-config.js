1e10604357be492042f22d8b825b5396ea1af7f7
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-19 00:43:42 -0500

    added polls class, Note:- fixed config issue

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,16 @@
+// module.exports = {
+//   // See <http://truffleframework.com/docs/advanced/configuration>
+//   // for more about customizing your Truffle configuration!
+//   networks: {
+//     development: {
+//       host: "127.0.0.1",
+//       port: 7545,
+//       network_id: "*" // Match any network id
+//     },
+    
+//   }
+// };
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -9,6 +22,11 @@ module.exports = {
     },
     develop: {
       port: 8545
+    },
+    compilers: {
+      solc: {
+        version: "0.5.12" // ex:  "0.4.20". (Default: Truffle's installed solc)
+      }
     }
   }
 };
