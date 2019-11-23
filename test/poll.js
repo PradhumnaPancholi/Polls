45d13bb4d594afec4ecc80518fe457d5a343bda1
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-11-23 17:57:28 -0500

    added test to very contest data from block

--- test/poll.js
+++ test/poll.js
@@ -2,8 +2,8 @@ const Poll  = artifacts.require('./Poll')
 
 //testing the smart contracts//
 
-// 1. Test to verify number of contestants//
 contract('Poll', (accounts) => {
+    // 1. Test to verify number of contestants//
     it('Initialize & Get Number of Contestant', () => {
         //check if the contract id deployed//
         return Poll.deployed().then((instance) => {
@@ -12,6 +12,24 @@ contract('Poll', (accounts) => {
         }).then((count) => {
             //test with assertion//
             assert.equal(count, 2)
+        })
+    })
+    // 2. Test to verify the contestant data stored in block//
+    it('Verifies if the contestants data in the blockchain', () => {
+        return Poll.deployed().then((instance) => {
+            pollInstance = instance
+            return pollInstance.contestants(1)
+        }).then((contestant) => {
+            assert.equal(contestant[0], 1, 'Id is correct')
+            assert.equal(contestant[1], 'FRIENDS', 'Name is correct')
+            assert.equal(contestant[2], 'Comedy', 'Genre is correct')
+            //return next contestant
+            return pollInstance.contestants(2)
+            //verify data
+        }).then((contestant) => {
+            assert.equal(contestant[0], 2, 'Id is correct')
+            assert.equal(contestant[1], 'HIMYM', 'Name is correct')
+            assert.equal(contestant[2], 'Comedy', 'Genre is correct')
         })
     })
 })
